09636da44a6f5aad712c8f361621f8f43e643517
tanweipeng <wptan9915@gmail.com>
2021-12-07 20:36:31 +0800

    [D2] Add point and score in user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,19 +4,25 @@ class UserModel {
   late final String about;
   late final String phoneNumber;
   late final String emailAddress;
+  late final int point;
+  late final int score;
 
-  UserModel.fromJson(Map<String, String> json)
-      : firstName = json['firstName']!,
-        lastName = json['lastName']!,
-        about = json['about']!,
-        phoneNumber = json['phoneNumber']!,
-        emailAddress = json['emailAddress']!;
+  UserModel.fromJson(Map<String, dynamic> json)
+      : firstName = json['firstName'],
+        lastName = json['lastName'],
+        about = json['about'],
+        phoneNumber = json['phoneNumber'],
+        emailAddress = json['emailAddress'],
+        point = json['point'],
+        score = json['score'];
 
-  Map<String, String> toMap() => {
+  Map<String, dynamic> toMap() => {
         "firstName": firstName,
         "lastName": lastName,
         "about": about,
         "phoneNumber": phoneNumber,
-        "emailAddress": emailAddress
+        "emailAddress": emailAddress,
+        "point": point,
+        "score": score
       };
 }
