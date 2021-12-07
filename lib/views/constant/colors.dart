85735c27c258da3560eb2219534ae613eba630de
aso <wid180039@siswa.um.edu.my>
2021-12-07 08:52:39 +0800

    added colors

---
+++ lib/views/constant/colors.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const Color WHITE_01 = Color(0xFFFFFFFF);
+const Color WHITE_02 = Color(0xFFF2F3FF);
+const Color BLUE_01 = Color(0xFF30466B);
+const Color BLUE_02 = Color(0xFF506CA4);
+const Color BLUE_03 = Color(0xFF778ACE);
+const Color RED_01 = Color(0xFFFF5454);
+const Color RED_02 = Color(0xFFFF7966);
+const Color YELLOW_01 = Color(0xFFFFD86E);
+const Color GREY_01 = Color(0xFF3F3F3F);
+const Color GREY_02 = Color(0xFF909090);
