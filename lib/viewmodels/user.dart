fa6ca0f7be4cf8904828bd3b54ab70539cf9e11a
tanweipeng <wptan9915@gmail.com>
2021-12-08 23:26:34 +0800

    [D3] Add model persistence

--- lib/viewmodels/user.dart
+++ lib/viewmodels/user.dart
@@ -1,9 +1,17 @@
 import 'package:habyte/models/user.dart';
+import 'package:habyte/viewmodels/general.dart';
+import 'package:habyte/views/constant/constants.dart';
 
 class User {
   static final User _user = User._internal();
   factory User.getInstance() => _user;
   User._internal();
+
+  BoxType boxType = BoxType.main;
+
+  String key = BOX_USER;
+
+  final General _general = General.getInstance();
 
   UserModel? currentUser;
 
@@ -14,11 +22,17 @@ class User {
   void updateCurrentUser(Map<String, dynamic> newUserJson) =>
       currentUser = UserModel.fromJson(newUserJson);
 
+  void editProfilePicture(String profilePicPath) {
+    currentUser!.profilePicPath = profilePicPath;
+    _general.updateBoxItem(boxType, key, currentUser!.toMap());
+  }
+
   void minusScore(int numOfScore) {
     if (currentUser!.scores >= numOfScore) {
       currentUser!.scores -= numOfScore;
     } else {
       currentUser!.scores = 0;
     }
+    _general.updateBoxItem(boxType, key, currentUser!.toMap());
   }
 }
