b1da3037f282fcb2236c08384d8f8a84c3152f16
aso <wid180039@siswa.um.edu.my>
2021-12-09 23:14:20 +0800

    rewards initial framework

--- lib/views/pages/rewards/_rewards.dart
+++ lib/views/pages/rewards/_rewards.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:habyte/views/constant/sizes.dart';
+import 'package:habyte/views/widgets/profile_score_card.dart';
+
 class RewardsPage extends StatelessWidget {
-  const RewardsPage
-({ Key? key }) : super(key: key);
+  const RewardsPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(child: Text('Rewards'),),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        SizedBox(height: TOP_PADDING, width: double.infinity),
+        ProfileScoreCard(),
+      ],
     );
   }
 }
