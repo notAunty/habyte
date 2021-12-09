90ec20673814aa3ecabc73eb372784e9212d6f14
tanweipeng <wptan9915@gmail.com>
2021-12-09 14:55:37 +0800

    [D2] & [D3] Change to statefulwidget and add pickImage from gallery as profile picture, & profile pic persistence in storage

--- lib/views/widgets/profile_picture.dart
+++ lib/views/widgets/profile_picture.dart
@@ -1,9 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:habyte/viewmodels/user.dart';
 import 'package:habyte/views/constant/colors.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ProfilePictureUpdate extends StatelessWidget {
-  const ProfilePictureUpdate({ Key? key }) : super(key: key);
+  const ProfilePictureUpdate({Key? key, required this.pickImage})
+      : super(key: key);
+
+  final Function pickImage;
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +19,53 @@ class ProfilePictureUpdate extends StatelessWidget {
       mini: true,
       heroTag: 'edit-profile-picture',
       backgroundColor: Colors.white,
-      child: const Icon(FeatherIcons.camera, size: 20,),
-      onPressed: () {},
+      child: const Icon(
+        FeatherIcons.camera,
+        size: 20,
+      ),
+      onPressed: () => pickImage(),
     );
   }
 }
 
-class ProfilePictureHolder extends StatelessWidget {
-  const ProfilePictureHolder({Key? key, this.editable = false, required this.initials, this.radius = 32}) : super(key: key);
+class ProfilePictureHolder extends StatefulWidget {
+  const ProfilePictureHolder({
+    Key? key,
+    this.editable = false,
+    required this.initials,
+    this.radius = 32,
+  }) : super(key: key);
 
   final bool editable;
   final double radius;
   final String initials;
 
   @override
+  _ProfilePictureHolderState createState() => _ProfilePictureHolderState();
+}
+
+class _ProfilePictureHolderState extends State<ProfilePictureHolder> {
+  final User _user = User.getInstance();
+
+  void _imgFromGallery() async {
+    XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    final Directory appDir = await getApplicationDocumentsDirectory();
+    final String filePath = '${appDir.path}/${image?.name}';
+    await image!.saveTo(filePath);
+    image = null;
+
+    setState(() {
+      _user.editProfilePicture(filePath);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    bool editable = widget.editable;
+    double radius = widget.radius;
+    String initials = widget.initials;
+
     return Stack(
       alignment: Alignment.center,
       children: [
@@ -65,22 +105,46 @@ class ProfilePictureHolder extends StatelessWidget {
                 ),
               ]),
         ),
-        Padding(
-          padding: EdgeInsets.only(top: radius * 0.15),
-          child: Text(
+        if (_user.currentUser != null)
+          if (_user.currentUser?.profilePicPath != null ||
+              _user.currentUser?.profilePicPath == "")
+            Container(
+              child: Image.file(
+                File(_user.currentUser?.profilePicPath ?? ""),
+                fit: BoxFit.cover,
+              ),
+              height: (radius - (radius * 0.05)) * 2,
+              width: (radius - (radius * 0.05)) * 2,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(radius),
+              ),
+            )
+          else
+            Text(
+              _user.currentUser!.name[0],
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: radius * 0.9),
+            )
+        else
+          Text(
             initials,
             style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.w600,
                 fontSize: radius * 0.9),
           ),
-        ),
 
-        if (editable) const Positioned(
-          right: 0,
-          bottom: 0,
-          child: ProfilePictureUpdate(),
-        ),
+        if (editable)
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: ProfilePictureUpdate(
+              pickImage: () => _imgFromGallery(),
+            ),
+          ),
       ],
     );
   }
