96682f69854d64aff91088e6ccb90b4ded345915
tanweipeng <wptan9915@gmail.com>
2021-12-09 19:23:18 +0800

    [D2] Enhance notification viewmodel with comments

--- lib/viewmodels/notifications.dart
+++ lib/viewmodels/notifications.dart
@@ -1,21 +1,109 @@
 import 'package:habyte/models/notification.dart';
 import 'package:habyte/viewmodels/general.dart';
 
+/// **Notification ViewModel Class**
+///
+/// Involves:
+/// - Notification Model
+/// - CRUD
+/// - Other operations
 class Notifications {
   static final Notifications _notifications = Notifications._internal();
-  factory Notifications.getInstance() => _notifications;
   Notifications._internal();
 
-  BoxType boxType = BoxType.notification;
+  /// Get the `Notification` instance for user `CRUD` and other operations
+  factory Notifications.getInstance() => _notifications;
 
   final General _general = General.getInstance();
+  final BoxType _boxType = BoxType.notification;
+  List<NotificationModel> _currentNotifications = [];
 
-  late final List<NotificationModel> _currentNotifications;
+  /// Everytime login, `retrievePreviousLogin()` in general need to call this
+  /// to insert the data stored.
+  void setCurrentNotifications(List<NotificationModel> notificationModelList) =>
+      _currentNotifications = notificationModelList;
 
-  void setCurrentNotifications(List<NotificationModel> notificationModelList) {
-    _currentNotifications = notificationModelList;
+  /// **Create Reward** (`C` in CRUD)
+  ///
+  /// Create notification will need to pass a map with the pairs of key and value
+  ///
+  /// Below are the keys for creating `Notification`:
+  /// - `NOTIFICATION_TASK_ID`
+  /// - `NOTIFICATION_TIME`
+  ///
+  /// **Remark:** Above keys are gotten from `constant.dart`. Kindly import
+  /// from there
+  void createNotification(Map<String, dynamic> notificationJson) {
+    NotificationModel _notificationModel =
+        NotificationModel.fromJson(notificationJson);
+    _notificationModel.id = _general.getBoxItemNewId(_boxType);
+    _currentNotifications.add(_notificationModel);
+    _general.addBoxItem(_boxType, _notificationModel.id, _notificationModel);
   }
 
+  /// **Retrieve Notification** (`R` in CRUD)
+  ///
+  /// Call this function when you need the info in `List of NotificationModel`.
+  List<NotificationModel> retrieveAllNotifications() => _currentNotifications;
+
+  /// **Retrieve Notification** (`R` in CRUD)
+  ///
+  /// Call this function when you need the info in `List of Map`
+  /// (converted from `NotificationModel`).
+  List<Map<String, dynamic>> retrieveAllNotificationsInListOfMap() =>
+      _toListOfMap();
+
+  /// **Retrieve Notification** (`R` in CRUD)
+  ///
+  /// Call this function when you need the info from one of the `NotificationModel`.
+  ///
+  /// Parameter required: `id` from `NotificationModel`.
+  NotificationModel retrieveNotificationById(String id) => _currentNotifications
+      .singleWhere((notificationModel) => notificationModel.id == id);
+
+  /// **Retrieve Notification** (`R` in CRUD)
+  ///
+  /// Call this function when you need the info from one of the `NotificationModel`.
+  ///
+  /// Parameter required: `taskId` from `TaskModel`.
+  NotificationModel retrieveNotificationByTaskId(String taskId) =>
+      _currentNotifications.singleWhere(
+          (notificationModel) => notificationModel.taskId == taskId);
+
+  /// **Update Notification** (`U` in CRUD)
+  ///
+  /// Update notification will just need to pass the `id` of the `NotificationModel`
+  /// & a map with the key and value that need to update
+  ///
+  /// Below are the fields that can be updated:
+  /// - `NOTIFICATION_TASK_ID`
+  /// - `NOTIFICATION_TIME`
+  ///
+  /// **Remark:** Above keys are gotten from `constant.dart`. Kindly import
+  /// from there
+  void updateNotification(String id, Map<String, dynamic> jsonToUpdate) {
+    int _index = _currentNotifications
+        .indexWhere((notificationModel) => notificationModel.id == id);
+    NotificationModel _updatedNotificationModel = NotificationModel.fromJson({
+      ..._currentNotifications[_index].toMap(),
+      ...jsonToUpdate,
+    });
+    _currentNotifications[_index] = _updatedNotificationModel;
+    _general.updateBoxItem(
+        _boxType, _updatedNotificationModel.id, _updatedNotificationModel);
+  }
+
+  /// **Delete Notification** (`D` in CRUD)
+  ///
+  /// Call this function when need to delete notification
+  void deleteNotification(String id) {
+    int index = _currentNotifications
+        .indexWhere((notificationModel) => notificationModel.id == id);
+    String removedId = _currentNotifications.removeAt(index).id;
+    _general.deleteBoxItem(_boxType, removedId);
+  }
+
+  /// Private function to convert `List of NotificationModel` to `List of Map`
   List<Map<String, dynamic>> _toListOfMap() {
     List<Map<String, dynamic>> notificationsInListOfMap = [];
     for (NotificationModel notificationModel in _currentNotifications) {
@@ -23,45 +111,4 @@ class Notifications {
     }
     return notificationsInListOfMap;
   }
-
-  //// CRUD
-  // C
-  void createNotification(NotificationModel notificationModel) {
-    notificationModel.id = _general.getBoxItemNewId(BoxType.notification);
-    _currentNotifications.add(notificationModel);
-    _general.addBoxItem(boxType, notificationModel.id, notificationModel);
-  }
-
-  // R
-  List<NotificationModel> retrieveAllNotifications() => _currentNotifications;
-  List<Map<String, dynamic>> retrieveAllNotificationsInListOfMap() =>
-      _toListOfMap();
-
-  // r
-  NotificationModel retrieveNotificationById(String id) => _currentNotifications
-      .singleWhere((notificationModel) => notificationModel.id == id);
-
-  NotificationModel retrieveNotificationByTaskId(String taskId) =>
-      _currentNotifications.singleWhere(
-          (notificationModel) => notificationModel.taskId == taskId);
-
-  // U
-  void updateNotification(
-      String id, NotificationModel updatedNotificationModel) {
-    int index = _currentNotifications
-        .indexWhere((notificationModel) => notificationModel.id == id);
-    updatedNotificationModel.id = _currentNotifications[index].id;
-    _currentNotifications[index] = updatedNotificationModel;
-    _general.updateBoxItem(
-        boxType, updatedNotificationModel.id, updatedNotificationModel);
-  }
-
-  // D
-  void deleteNotification(String id) {
-    int index = _currentNotifications
-        .indexWhere((notificationModel) => notificationModel.id == id);
-    String removedId = _currentNotifications.removeAt(index).id;
-    _general.deleteBoxItem(boxType, removedId);
-  }
-  ////
 }
