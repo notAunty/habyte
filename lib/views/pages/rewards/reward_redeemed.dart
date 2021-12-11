ff5dcc32cfd57779ee4a35967560a5e7ab520635
J W <jwooon03+heroku@protonmail.com>
2021-12-11 18:40:03 +0800

    added reward_list and reward_redeemed

---
+++ lib/views/pages/rewards/reward_redeemed.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:habyte/views/constant/colors.dart';
+
+class RewardRedeemed extends StatelessWidget {
+  const RewardRedeemed({Key? key, required this.points, required this.name})
+      : super(key: key);
+
+  final int points;
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.5,
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        horizontalTitleGap: 0,
+        leading: Container(
+          width: 64,
+          color: GREY_02.withOpacity(0.5),
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  points.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Text('pts'),
+              ],
+            ),
+          ),
+        ),
+        tileColor: WHITE_01,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(name, style: Theme.of(context).textTheme.headline6),
+        ),
+      ),
+    );
+  }
+}
