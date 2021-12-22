faa27e565b33ea2f438cf2e9e34d4723e7183b4d
aso <wid180039@siswa.um.edu.my>
2021-12-22 15:10:25 +0800

    almost done the ui

--- lib/views/pages/rewards/reward_list_redeemed.dart
+++ lib/views/pages/rewards/reward_list_redeemed.dart
@@ -3,23 +3,15 @@ import 'package:habyte/views/constant/colors.dart';
 import 'package:habyte/views/constant/sizes.dart';
 import 'package:habyte/views/pages/rewards/reward_item.dart';
 
-class RewardList extends StatelessWidget {
-  const RewardList({Key? key}) : super(key: key);
+class RewardListRedeemed extends StatelessWidget {
+  const RewardListRedeemed({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // Mock
-    final List<int> rewardPoint = [5, 10, 15, 20, 25, 30, 5, 5, 5, 10];
+    final List<int> rewardPoint = [5, 10];
 
     final List<String> rewardName = [
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
-      "Have a cheat meal at MCDonald's",
       "Have a cheat meal at MCDonald's",
       "Visit an Art Exhibition"
     ];
@@ -36,7 +28,7 @@ class RewardList extends StatelessWidget {
               vertical: 16,
               horizontal: SIDE_PADDING,
             ),
-            itemBuilder: (context, index) => RewardItem(
+            itemBuilder: (context, index) => RedeemedRewardItem(
               points: rewardPoint[index],
               name: rewardName[index],
             ),
