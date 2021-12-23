ca117f424a22dc52a39c450042af4f6d8437c644
tanweipeng <wptan9915@gmail.com>
2021-12-23 22:14:37 +0800

    [Object model] Add one more field - available to differentiate available reward and redeemed reward

--- lib/models/hiveAdapter/reward.g.dart
+++ lib/models/hiveAdapter/reward.g.dart
@@ -19,19 +19,22 @@ class RewardAdapter extends TypeAdapter<Reward> {
     return Reward()
       ..id = fields[0] as String
       ..name = fields[1] as String
-      ..points = fields[2] as int;
+      ..points = fields[2] as int
+      ..available = fields[3] as bool;
   }
 
   @override
   void write(BinaryWriter writer, Reward obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(4)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.name)
       ..writeByte(2)
-      ..write(obj.points);
+      ..write(obj.points)
+      ..writeByte(3)
+      ..write(obj.available);
   }
 
   @override
--- lib/models/reward.dart
+++ lib/models/reward.dart
@@ -15,20 +15,26 @@ class Reward {
   @HiveField(2)
   late final int points;
 
+  @HiveField(3)
+  late final bool available;
+
   Reward();
 
   Reward.fromJson(Map<String, dynamic> json)
-      : name = json[REWARD_NAME]!,
-        points = json[REWARD_POINTS]!;
+      : name = json[REWARD_NAME],
+        points = json[REWARD_POINTS],
+        available = json[REWARD_AVAILABLE];
 
   Map<String, dynamic> toMap() => {
         REWARD_ID: id,
         REWARD_NAME: name,
         REWARD_POINTS: points,
+        REWARD_AVAILABLE: available
       };
 
   Reward nullClass() => Reward()
     ..id = NULL_STRING_PLACEHOLDER
     ..name = NULL_STRING_PLACEHOLDER
-    ..points = NULL_INT_PLACEHOLDER;
+    ..points = NULL_INT_PLACEHOLDER
+    ..available = NULL_BOOL_PLACEHOLDER;
 }
