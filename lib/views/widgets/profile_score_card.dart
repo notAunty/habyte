78cd1e301bfc9c1cb128d12140a07d02a5950840
tanweipeng <wptan9915@gmail.com>
2021-12-23 22:16:39 +0800

    [D7] Added value notifier and value listenable builder

--- lib/views/widgets/profile_score_card.dart
+++ lib/views/widgets/profile_score_card.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:habyte/utils/membership_tier.dart';
 import 'package:habyte/viewmodels/taskEntry.dart';
 import 'package:habyte/viewmodels/user.dart';
 import 'package:habyte/views/constant/colors.dart';
 import 'package:habyte/views/constant/constants.dart';
 import 'package:habyte/views/constant/sizes.dart';
+import 'package:habyte/views/widgets/double_value_listenable_builder.dart';
 import 'package:habyte/views/widgets/profile_picture.dart';
 
 class ProfileScoreCard extends StatelessWidget {
@@ -21,143 +23,148 @@ class ProfileScoreCard extends StatelessWidget {
     final String _userFirstName = _userVM.retrieveUser()!.toMap()[USER_FIRST_NAME];
     final String _userLastName =  _userVM.retrieveUser()!.toMap()[USER_LAST_NAME];
     final String _userName = _userFirstName + ' ' + _userLastName;
-    final int? _userScore = _userVM.retrieveUser()!.toMap()[USER_SCORES];
-    final int _numOfEntries = _taskEntryVM.retrieveAllTaskEntries().length;
 
-    return Hero(
-      tag: heroTag ?? Random.secure().nextDouble(),
-      child: Padding(
-        padding:
-            const EdgeInsets.symmetric(horizontal: SIDE_PADDING, vertical: 8),
-        child: Container(
-          height: 160,
-          decoration: BoxDecoration(
-            color: MEMBERSHIP_COLOR[Random().nextInt(MEMBERSHIP_COLOR.length)],
-            borderRadius: BorderRadius.circular(BORDER_RADIUS),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Spacer(),
-              Row(
+    return DoubleValueListenableBuilder<int, int>(
+      firstValueListenable: _userVM.getScoreNotifier(),
+      secondValueListenable: _taskEntryVM.getNumOfEntriesNotifier(),
+      builder: (context, firstValue, secondValue) {
+        return Hero(
+          tag: heroTag ?? Random.secure().nextDouble(),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: SIDE_PADDING, vertical: 8),
+            child: Container(
+              height: 160,
+              decoration: BoxDecoration(
+                // color: MEMBERSHIP_COLOR[Random().nextInt(MEMBERSHIP_COLOR.length)],
+                color: getCardColorByScores(firstValue),
+                borderRadius: BorderRadius.circular(BORDER_RADIUS),
+              ),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(SIDE_PADDING * 0.75, 0, 16, 0),
-                    child: ProfilePictureHolder(radius: 28),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        _userName,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline4!
-                            .copyWith(color: WHITE_01),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(SIDE_PADDING * 0.75, 0, 16, 0),
+                        child: ProfilePictureHolder(radius: 28),
                       ),
-                      Opacity(
-                        opacity: 0.7,
-                        child: Text(
-                          // TODO
-                          'Novice',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(color: WHITE_01),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: SIDE_PADDING * 0.75),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          _userScore.toString(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline4!
-                              .copyWith(color: WHITE_01),
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          'pts',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(color: WHITE_01),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          _numOfEntries.toString(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline4!
-                              .copyWith(color: WHITE_01),
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          'entries',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(color: WHITE_01),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: SIDE_PADDING * 0.75),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 12,
-                      child: Row(
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.asset('assets/logo/white.png'),
-                          const SizedBox(width: 2),
                           Text(
-                            'Habyte',
+                            _userName,
                             style: Theme.of(context)
                                 .textTheme
-                                .subtitle2!
+                                .headline4!
                                 .copyWith(color: WHITE_01),
+                          ),
+                          Opacity(
+                            opacity: 0.7,
+                            child: Text(
+                              // TODO
+                              getMembershipTierByScores(firstValue),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(color: WHITE_01),
+                            ),
                           ),
                         ],
                       ),
-                    )
-                  ],
-                ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: SIDE_PADDING * 0.75),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              firstValue.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4!
+                                  .copyWith(color: WHITE_01),
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              'pts',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(color: WHITE_01),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              secondValue.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4!
+                                  .copyWith(color: WHITE_01),
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              'entries',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(color: WHITE_01),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: SIDE_PADDING * 0.75),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          height: 12,
+                          child: Row(
+                            children: [
+                              Image.asset('assets/logo/white.png'),
+                              const SizedBox(width: 2),
+                              Text(
+                                'Habyte',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle2!
+                                    .copyWith(color: WHITE_01),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: SIDE_PADDING * 0.75,
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: SIDE_PADDING * 0.75,
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
