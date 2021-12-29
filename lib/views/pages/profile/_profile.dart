586e0779c0d1ddceb10467e08ff7a65e427940e4
tanweipeng <wptan9915@gmail.com>
2021-12-30 00:33:39 +0800

    [D5] Link Profile page with backend

--- lib/views/pages/profile/_profile.dart
+++ lib/views/pages/profile/_profile.dart
@@ -14,66 +14,78 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String _userFirstName = _userVM.retrieveUser()!.firstName;
-    final String _userLastName = _userVM.retrieveUser()!.lastName;
-    final String about = _userVM.retrieveUser()!.about ?? '';
-
     return Scaffold(
       body: SafeArea(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: TOP_PADDING, width: double.infinity),
-              const ProfileHeader(),
-              const SizedBox(height: TOP_PADDING, width: double.infinity),
-              Column(
-                children: [
-                  const SizedBox(
-                    width: double.infinity,
-                  ),
-                  const ProfilePictureHolder(radius: 64),
-                  const SizedBox(height: TOP_PADDING),
-                  Text(
-                    '$_userFirstName $_userLastName',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ],
-              ),
-              const SizedBox(height: TOP_PADDING),
-              Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width - 32,
-                  child: ElevatedButton(
-                    child: const Text('Edit Profile'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const EditProfilePage()),
-                      );
-                    },
+        child: StatefulBuilder(
+          builder: (context, setState) {
+            final String _userFirstName = _userVM.retrieveUser()!.firstName;
+            final String _userLastName = _userVM.retrieveUser()!.lastName;
+            final String _userName = _userFirstName + ' ' + _userLastName;
+            final String about = _userVM.retrieveUser()!.about ?? '';
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: TOP_PADDING, width: double.infinity),
+                const ProfileHeader(),
+                const SizedBox(height: TOP_PADDING, width: double.infinity),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: SIDE_PADDING),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        width: double.infinity,
+                      ),
+                      const ProfilePictureHolder(radius: 64),
+                      const SizedBox(height: TOP_PADDING),
+                      Text(
+                        _userName,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(height: TOP_PADDING),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: SIDE_PADDING),
-                child: Column(
+                const SizedBox(height: TOP_PADDING),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width - 32,
+                    child: ElevatedButton(
+                      child: const Text('Edit Profile'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EditProfilePage(
+                                    setState: setState,
+                                  )),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: TOP_PADDING),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: SIDE_PADDING),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'About',
-                      style: Theme.of(context).textTheme.overline,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      about,
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
-                  ],
+                    children: [
+                      Text(
+                        'About',
+                        style: Theme.of(context).textTheme.overline,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        about,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ]),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
--- lib/views/pages/profile/profile_edit.dart
+++ lib/views/pages/profile/profile_edit.dart
@@ -4,16 +4,12 @@ import 'package:habyte/views/widgets/profile_picture.dart';
 import 'package:habyte/viewmodels/user.dart';
 import 'package:habyte/views/constant/constants.dart';
 import 'package:habyte/views/widgets/text_fields.dart';
-import 'package:habyte/views/pages/profile/_profile.dart';
 
-class EditProfilePage extends StatefulWidget {
-  const EditProfilePage({Key? key}) : super(key: key);
+class EditProfilePage extends StatelessWidget {
+  EditProfilePage({Key? key, required this.setState}) : super(key: key);
 
-  @override
-  State<EditProfilePage> createState() => _EditProfilePageState();
-}
-
-class _EditProfilePageState extends State<EditProfilePage> {
+  final Function setState;
+  final GlobalKey<FormState> _formKey = GlobalKey();
   final UserVM _userVM = UserVM.getInstance();
 
   void addTempFirstName(String firstName) =>
@@ -25,12 +21,12 @@ class _EditProfilePageState extends State<EditProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController firstNameController =
-        TextEditingController(text: _userVM.retrieveUser()!.firstName);
-    TextEditingController lastNameController =
-        TextEditingController(text: _userVM.retrieveUser()!.lastName);
+    TextEditingController firstNameController = TextEditingController(
+        text: _userVM.retrieveTempUserJson()[USER_FIRST_NAME]);
+    TextEditingController lastNameController = TextEditingController(
+        text: _userVM.retrieveTempUserJson()[USER_LAST_NAME]);
     TextEditingController aboutController =
-        TextEditingController(text: _userVM.retrieveUser()!.about);
+        TextEditingController(text: _userVM.retrieveTempUserJson()[USER_ABOUT]);
 
     return Scaffold(
       appBar: AppBar(),
@@ -38,58 +34,63 @@ class _EditProfilePageState extends State<EditProfilePage> {
         physics: const BouncingScrollPhysics(),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: SIDE_PADDING),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: TOP_PADDING, width: double.infinity),
-              const ProfilePictureHolder(
-                radius: 64,
-                editable: true,
-              ),
-              const SizedBox(height: 24),
-              CustomTextFieldLabel(
-                label: 'First Name*',
-                child: CustomTextField(
-                  maxWords: -1,
-                  isRequired: true,
-                  controller: firstNameController,
-                  onChanged: (value) => addTempFirstName(value),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: TOP_PADDING, width: double.infinity),
+                const ProfilePictureHolder(
+                  radius: 64,
+                  editable: true,
                 ),
-              ),
-              const SizedBox(height: 24),
-              CustomTextFieldLabel(
-                label: 'Last Name*',
-                child: CustomTextField(
-                  maxWords: -1,
-                  isRequired: true,
-                  controller: lastNameController,
-                  onChanged: (value) => addTempLastName(value),
+                const SizedBox(height: 24),
+                CustomTextFieldLabel(
+                  label: 'First Name*',
+                  child: CustomTextField(
+                    maxWords: -1,
+                    isRequired: true,
+                    controller: firstNameController,
+                    onChanged: (value) => addTempFirstName(value),
+                  ),
                 ),
-              ),
-              const SizedBox(height: TOP_PADDING),
-              CustomTextFieldLabel(
-                label: 'About',
-                child: CustomTextField(
-                  maxWords: -1,
-                  isRequired: false,
-                  controller: aboutController,
-                  onChanged: (about) => addTempAbout(about),
-                  isMultiline: true,
+                const SizedBox(height: 24),
+                CustomTextFieldLabel(
+                  label: 'Last Name*',
+                  child: CustomTextField(
+                    maxWords: -1,
+                    isRequired: true,
+                    controller: lastNameController,
+                    onChanged: (value) => addTempLastName(value),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 32,
-                child: ElevatedButton(
-                  child: const Text('Done'),
-                  onPressed: () {
-                    _userVM.updateUser(_userVM.retrieveTempUserJson());
-                    Navigator.pop(context);
-                  },
+                const SizedBox(height: TOP_PADDING),
+                CustomTextFieldLabel(
+                  label: 'About',
+                  child: CustomTextField(
+                    maxWords: -1,
+                    isRequired: false,
+                    controller: aboutController,
+                    onChanged: (about) => addTempAbout(about),
+                    isMultiline: true,
+                  ),
                 ),
-              ),
-              const SizedBox(height: TOP_PADDING),
-            ],
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 32,
+                  child: ElevatedButton(
+                    child: const Text('Done'),
+                    onPressed: () {
+                      setState(() {
+                        _userVM.updateUser(_userVM.retrieveTempUserJson());
+                        Navigator.pop(context);
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: TOP_PADDING),
+              ],
+            ),
           ),
         ),
       ),
