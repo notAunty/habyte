fb3e4445fa6738c646b4044b91206cc521be17a4
tanweipeng <wptan9915@gmail.com>
2022-01-11 13:50:43 +0800

    Pass the about to ProfileAbout

--- lib/views/pages/profile/profile_about.dart
+++ lib/views/pages/profile/profile_about.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:habyte/viewmodels/user.dart';
 
 class ProfileAbout extends StatelessWidget {
-  const ProfileAbout({Key? key}) : super(key: key);
+  const ProfileAbout({Key? key, required this.about}) : super(key: key);
+
+  final String about;
 
   @override
   Widget build(BuildContext context) {
-    final UserVM _userVM = UserVM.getInstance();
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text('About'.toUpperCase(), style: Theme.of(context).textTheme.overline),
         const SizedBox(height: 8),
-        Text(_userVM.retrieveUser()!.about ?? '',
-            style: Theme.of(context).textTheme.bodyText2),
+        Text(about, style: Theme.of(context).textTheme.bodyText2),
       ],
     );
   }
