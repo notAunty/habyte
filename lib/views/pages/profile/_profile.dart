1dfdc906480e3b9b27c450b604a485708008966f
tanweipeng <wptan9915@gmail.com>
2022-01-11 13:50:08 +0800

    Add ValueListenableBuilder to sync userName & userAbout after profile edit

--- lib/views/pages/profile/_profile.dart
+++ lib/views/pages/profile/_profile.dart
@@ -1,56 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
-import 'package:habyte/viewmodels/user.dart';
-
+import 'package:habyte/viewmodels/notifiers.dart';
 import 'package:habyte/views/constant/sizes.dart';
 import 'package:habyte/views/pages/profile/profile_about.dart';
 import 'package:habyte/views/pages/profile/profile_header.dart';
 import 'package:habyte/views/pages/profile/profile_reminders.dart';
 import 'package:habyte/views/pages/profile/profile_settings.dart';
+import 'package:habyte/views/widgets/double_value_listenable_builder.dart';
 import 'package:habyte/views/widgets/profile_picture.dart';
-import 'package:habyte/views/pages/profile/profile_edit.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({Key? key}) : super(key: key);
 
-  final UserVM _userVM = UserVM.getInstance();
+  final Notifiers _notifiers = Notifiers.getInstance();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: SIDE_PADDING, vertical: TOP_PADDING),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const ProfileHeader(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const ProfilePictureHolder(radius: 64),
-                    const SizedBox(height: TOP_PADDING),
-                    Text(
-                      "${_userVM.retrieveUser()!.firstName} ${_userVM.retrieveUser()!.lastName}",
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.headline5,
+    return DoubleValueListenableBuilder<String, String>(
+      firstValueListenable: _notifiers.getNameNotifier(),
+      secondValueListenable: _notifiers.getAboutNotifier(),
+      builder: (context, userName, userAbout) {
+        return Scaffold(
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: SIDE_PADDING, vertical: TOP_PADDING),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const ProfileHeader(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const ProfilePictureHolder(radius: 64),
+                        const SizedBox(height: TOP_PADDING),
+                        Text(
+                          userName,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
                     ),
+                    const SizedBox(height: TOP_PADDING),
+                    ProfileAbout(about: userAbout,),
+                    const SizedBox(height: TOP_PADDING),
+                    const Divider(),
+                    const SizedBox(height: 8),
+                    const ProfileSettings(),
+                    const SizedBox(height: TOP_PADDING),
+                    ProfileReminders(),
                   ],
                 ),
-                const SizedBox(height: TOP_PADDING),
-                const ProfileAbout(),
-                const SizedBox(height: TOP_PADDING),
-                const Divider(),
-                const SizedBox(height: 8),
-                const ProfileSettings(),
-                const SizedBox(height: TOP_PADDING),
-                ProfileReminders(),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
