6855ca137e2bd0763d51ac901b5aafb7411de325
aso <wid180039@siswa.um.edu.my>
2022-01-14 23:00:29 +0800

    profile tab scroll full

--- lib/views/pages/profile/profile_reminders.dart
+++ lib/views/pages/profile/profile_reminders.dart
@@ -16,97 +16,85 @@ class ProfileReminders extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text('Reminders'.toUpperCase(),
-              style: Theme.of(context).textTheme.overline),
-          ValueListenableBuilder<List<Map<String, dynamic>>>(
-            valueListenable: _notifiers.getReminderEntriesNotifier(),
-            builder: (context, reminderEntriesList, child) {
-              return reminderEntriesList.isNotEmpty
-                  ? Expanded(
-                      child: SingleChildScrollView(
-                        child: ListView.builder(
-                          primary: false,
-                          shrinkWrap: true,
-                          itemCount: reminderEntriesList.length,
-                          itemBuilder: (context, index) {
-                            Map<String, dynamic> currentReminderEntryInMap =
-                                reminderEntriesList[
-                                    reminderEntriesList.length - 1 - index];
-                            Task currentTask = _taskVM.retrieveTaskById(
-                              currentReminderEntryInMap[REMINDER_ENTRY_TASK_ID],
-                            );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text('Reminders'.toUpperCase(),
+            style: Theme.of(context).textTheme.overline),
+        ValueListenableBuilder<List<Map<String, dynamic>>>(
+          valueListenable: _notifiers.getReminderEntriesNotifier(),
+          builder: (context, reminderEntriesList, child) {
+            return reminderEntriesList.isNotEmpty
+                ? ListView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    itemCount: reminderEntriesList.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> currentReminderEntryInMap =
+                          reminderEntriesList[
+                              reminderEntriesList.length - 1 - index];
+                      Task currentTask = _taskVM.retrieveTaskById(
+                        currentReminderEntryInMap[REMINDER_ENTRY_TASK_ID],
+                      );
 
-                            String taskName = currentTask.name;
-                            TimeOfDay reminderTime =
-                                currentReminderEntryInMap[REMINDER_ENTRY_TIME];
-                            String reminderTimeInString =
-                                timeOfDayFormatter(reminderTime);
-                            if (currentTask.startDate.isAfter(DateTime.now())) {
-                              reminderTimeInString +=
-                                  " (Start on ${dateFormatterWithYYYYMMDD(currentTask.startDate)})";
-                            }
-                            bool reminderBool = currentReminderEntryInMap[
-                                REMINDER_ENTRY_STATUS];
+                      String taskName = currentTask.name;
+                      TimeOfDay reminderTime =
+                          currentReminderEntryInMap[REMINDER_ENTRY_TIME];
+                      String reminderTimeInString =
+                          timeOfDayFormatter(reminderTime);
+                      if (currentTask.startDate.isAfter(DateTime.now())) {
+                        reminderTimeInString +=
+                            " (Start on ${dateFormatterWithYYYYMMDD(currentTask.startDate)})";
+                      }
+                      bool reminderBool =
+                          currentReminderEntryInMap[REMINDER_ENTRY_STATUS];
 
-                            if (currentReminderEntryInMap[
-                                    REMINDER_ENTRY_TEMP_OFF_DATE] !=
-                                null) {
-                              if (isToday(currentReminderEntryInMap[
-                                  REMINDER_ENTRY_TEMP_OFF_DATE])) {
-                                reminderBool = false;
-                              }
-                            }
+                      if (currentReminderEntryInMap[
+                              REMINDER_ENTRY_TEMP_OFF_DATE] !=
+                          null) {
+                        if (isToday(currentReminderEntryInMap[
+                            REMINDER_ENTRY_TEMP_OFF_DATE])) {
+                          reminderBool = false;
+                        }
+                      }
 
-                            return ListTile(
-                              dense: true,
-                              contentPadding: EdgeInsets.zero,
-                              title: Text(
-                                taskName,
-                                style: Theme.of(context).textTheme.bodyText2,
-                              ),
-                              subtitle: Opacity(
-                                opacity: 0.7,
-                                child: Text(
-                                  reminderTimeInString,
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                              ),
-                              trailing: Switch(
-                                value: reminderBool,
-                                activeColor:
-                                    Theme.of(context).colorScheme.primary,
-                                onChanged: (_) {
-                                  _reminderEntryVM.updateReminderEntry(
-                                    currentReminderEntryInMap[
-                                        REMINDER_ENTRY_ID],
-                                    {
-                                      REMINDER_ENTRY_TEMP_OFF_DATE:
-                                          reminderBool ? DateTime.now() : null,
-                                    },
-                                  );
-                                },
-                              ),
+                      return ListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        title: Text(
+                          taskName,
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        subtitle: Opacity(
+                          opacity: 0.7,
+                          child: Text(
+                            reminderTimeInString,
+                            style: Theme.of(context).textTheme.subtitle2,
+                          ),
+                        ),
+                        trailing: Switch(
+                          value: reminderBool,
+                          activeColor: Theme.of(context).colorScheme.primary,
+                          onChanged: (_) {
+                            _reminderEntryVM.updateReminderEntry(
+                              currentReminderEntryInMap[REMINDER_ENTRY_ID],
+                              {
+                                REMINDER_ENTRY_TEMP_OFF_DATE:
+                                    reminderBool ? DateTime.now() : null,
+                              },
                             );
                           },
                         ),
-                      ),
-                    )
-                  : Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: const [
-                        SizedBox(height: TOP_PADDING),
-                        Text("No reminder available yet"),
-                      ],
-                    );
-            },
-          ),
-        ],
-      ),
+                      );
+                    },
+                  )
+                : const Padding(
+                    padding: EdgeInsets.only(top: TOP_PADDING),
+                    child: Text("No reminder available yet"),
+                  );
+          },
+        ),
+      ],
     );
   }
 }
